ce1f8f75bc663828b8cda62bb46b2dea5b8abbff
Vitor Linaris <vitor.linaris@gmail.com>
2025-07-30 01:07:05 -0300

    add a new version

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,7 +1,1 @@
-const track = document.querySelector(".logo-marquee__track");
-const group = document.querySelector(".logo-marquee__group");
 
-for (let i = 0; i < 4; i++) {
-  const clone = group.cloneNode(true);
-  track.appendChild(clone);
-}
